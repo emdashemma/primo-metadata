8542983d14a96c648d0b07153b547916ad645ac5
Emma Clarkson <75695487+emdashemma@users.noreply.github.com>
2022-04-30 18:13:20 -0700

    Add files via upload

---
+++ mapping.py
@@ -0,0 +1,39 @@
+## this script should be run from a directory that includes:
+##		(1) file.csv	export of Primo record
+##		(2) blurb.txt	paragraph about OMC to follow item-level metadata
+## and will create a txt file that can be pasted into the Internet Archive
+## "description" field.
+
+import csv
+
+csvfile = open('file.csv', newline='')
+obj = csv.reader(csvfile)
+
+count = 0
+for row in obj:
+	if count == 0:
+		header = row
+	else:
+		data = row
+	count += 1
+
+full_record = dict(zip(header, data))
+
+# any field from the csv can be listed in this dictionary to be included in the output txt file
+to_include = ['Title', 'Language','Format', 'Catalog Title', 'Publisher', 'Publication Date', 'Performer or Participant', 'OCLC Number', 'Permalink']
+
+selected_metadata = {}
+
+for term in to_include:
+	if term in full_record:
+		selected_metadata[term] = full_record[term]
+
+oclc = selected_metadata.get('OCLC Number')
+
+with open("blurb.txt", "r") as f:
+	blurb = f.read()
+
+with open(f"{oclc}.txt", "w") as f:
+	for key, value in selected_metadata.items():
+		f.write('%s: %s\n\n' % (key, value))
+	f.write(f"{blurb}")
